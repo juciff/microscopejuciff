e8cd25fc6fd7c85af852b60c3596c2bd39e79ae6
juciff <juanpablo123-@hotmail.com>
2014-09-30 18:22:47 -0500

    finalizacion de gitHub y collections

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,25 +1,5 @@
-var postsData = [
-  {
-
-    title: 'Introducing Telescope',
-    author: 'Sacha greif',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-  },
-
-  {
-
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
