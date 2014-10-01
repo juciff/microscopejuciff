088d835b8838c0cc63fad44c010cb966f04b4948
juciff <juanpablo123-@hotmail.com>
2014-10-01 17:54:18 -0500

    finalizacion de publicaciones y suscripciones

--- client/main.js
+++ client/main.js
@@ -1,1 +1,9 @@
 Meteor.subscribe('posts');
+
+Meteor.subscribe('posts','bob-smith');
+
+Template.posts.helpers({
+	posts: function (){
+		return Posts.find(author: 'bob-smith', category: 'JavaScript');
+	}
+});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,12 @@
 Meteor.publish('posts', function() {
   return Posts.find();
 });
+
+Meteor.publish('posts', function(){
+	return Posts.find({flagged: false});
+});
+
+Meteor.posts('posts', function(author){
+	return Posts.find({flagged: false, author:author});
+});
+
